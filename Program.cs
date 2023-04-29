15d181e9839021ae4f48fca3b1e2c851711fdf14
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 06:43:10 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BrojanjeStringa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string strSource = "";
+            int nCount = 0;
+
+            Console.WriteLine("Molimo vas da upisete zeljeni string: ");
+            strSource = Console.ReadLine();
+
+            foreach (char c in strSource)
+            {
+                nCount = nCount + 1;
+            }
+
+            Console.WriteLine("{0} length is {1}", strSource, nCount.ToString());
+            Console.ReadLine();
+
+
+        }
+    }
+}
